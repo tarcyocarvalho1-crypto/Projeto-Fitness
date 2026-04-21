48ae24323897e9dedb94e686c988891bd0ee9015
tarcyocarvalho1-crypto <tarcyocarvalho1@gmail.com>
2026-04-21 13:39:56 -0300

    novos itens adicionados

--- teste/index.js
+++
@@ -1,8 +0,0 @@
-const filtro = document.querySelector('feTurbulence');
-function animarRaio(){
-     const novaSemente = Math.random() * 100;
-    if (filtro){filtro.setAttribute('seed', novaSemente);
-        requestAnimationFrame(animarRaio);
-    }
-    }
-    animarRaio();
