79fa32b663904a46690162e09a856e98d370b583
tarcyocarvalho1-crypto <tarcyocarvalho1@gmail.com>
2026-05-10 14:23:27 -0300

    Fix cadastro: names do form e Supabase module

--- formulario.js
+++ formulario.js
@@ -12,14 +12,14 @@ formulario.addEventListener('submit', async (e) => {
     
     // Organiza os dados para o formato que o banco de dados entende
     const dadosParaEnviar = {
-        nome: dadosForm.get('Nome'),
+        nome: dadosForm.get('nome'),
         email: dadosForm.get('email'),
-        senha: dadosForm.get('Senha'),
-        data: dadosForm.get('Data'),
-       contato: dadosForm.get('Contato'),
-        cpf: dadosForm.get('CPF'),
-        peso: Number(dadosForm.get('Peso')),
-        altura: Number(dadosForm.get('Altura'))
+        senha: dadosForm.get('senha'),
+        data: dadosForm.get('data'),
+        contato: dadosForm.get('contato'),
+        cpf: dadosForm.get('cpf'),
+        peso: dadosForm.get('peso') ? Number(dadosForm.get('peso')) : null,
+        altura: dadosForm.get('altura') ? Number(dadosForm.get('altura')) : null
     };
 
     // Envia para a sua tabela no banco de dados
